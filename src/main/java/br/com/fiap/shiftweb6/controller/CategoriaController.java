dc8f8aa63558c6ea51de67d71a4e3821b47bff04
Flavio Moreni <proflavio.moreni@fiap.com.br>
2022-04-13 22:02:46 -0300

    consultas personalizadas

---
+++ src/main/java/br/com/fiap/shiftweb6/controller/CategoriaController.java
@@ -0,0 +1,56 @@
+package br.com.fiap.shiftweb6.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.fiap.shiftweb6.model.CategoriaModel;
+import br.com.fiap.shiftweb6.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categoria")
+public class CategoriaController {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<CategoriaModel> findById(@PathVariable("id") Long id) {
+		CategoriaModel categoriaModel = categoriaRepository.findById(id).orElse(null);
+		
+		if ( null == categoriaModel ) {
+			return ResponseEntity.notFound().build();
+		} else {
+			return ResponseEntity.ok(categoriaModel);
+		}
+		
+	}
+	
+	@GetMapping()
+	public ResponseEntity<List<CategoriaModel>> findAll() {
+		return ResponseEntity.ok( categoriaRepository.findAll() );	
+	}
+	
+	
+	@GetMapping("/total")
+	public ResponseEntity<Long> getTotalCategorias() {
+		return ResponseEntity.ok(categoriaRepository.getTotalCategorias());
+	}
+	
+	
+	@GetMapping("/nome")
+	public ResponseEntity<List<CategoriaModel>> findByNome(
+			@RequestParam("nome") String nome) {
+		
+		return ResponseEntity.ok( categoriaRepository.findByNomeCategoriaContaining(nome) );
+		
+	}
+	
+	
+}
---
+++ src/main/java/br/com/fiap/shiftweb6/repository/CategoriaRepository.java
@@ -0,0 +1,32 @@
+package br.com.fiap.shiftweb6.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import br.com.fiap.shiftweb6.model.CategoriaModel;
+
+public interface CategoriaRepository extends JpaRepository<CategoriaModel, Long> {
+
+	static final String QUERY_TOTAL_CATEGORIA = "SELECT COUNT(ATIVO) QUANT FROM SHIFT6_CATEGORIA WHERE ATIVO = 1";
+	
+	
+	@Override
+	@Query(value="SELECT c FROM CategoriaModel c WHERE ativo = 1")
+	public List<CategoriaModel> findAll();
+	
+	
+	@Override
+	@Query(value="SELECT c FROM CategoriaModel c WHERE ativo = 1 AND idCategoria = :id")
+	public Optional<CategoriaModel> findById(Long id);
+	
+	
+	@Query(	value=QUERY_TOTAL_CATEGORIA, nativeQuery = true)
+	public Long getTotalCategorias();
+	
+	
+	public List<CategoriaModel> findByNomeCategoriaContaining(String nome);
+	
+}
